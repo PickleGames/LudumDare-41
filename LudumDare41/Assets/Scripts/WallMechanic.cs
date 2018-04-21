c042c6078f1e8f19a9d81b51321d2cb6900466e0
Howarddful123 <Howarddful123@gmail.com>
2018-04-21 01:19:58 -0700

    -Add walls, wall jump.

--- LudumDare41/Assets/Scripts/WallMechanic.cs
+++ LudumDare41/Assets/Scripts/WallMechanic.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WallMechanic : MonoBehaviour {
+    public float moveSpeed;
+    private Transform target;
+
+
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        this.transform.Translate(moveSpeed, 0, 0);
+    }
+
+    void LateUpdate()
+    {
+        this.transform.position = new Vector2(this.transform.position.x, target.transform.position.y);
+    }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player Dieeedddd");
+            moveSpeed = 0;
         }
     }
+
+
 }
