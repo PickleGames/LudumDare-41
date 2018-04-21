6dec34f3193c4c24770e2f5fb31e8d1a16131d09
Miguel Garnica <mgarnica123@aol.com>
2018-04-20 21:25:08 -0700

    PlayerMovement
    Added:
        - PlayerMovement script
        - Player and Platfrom prefabs

---
+++ LudumDare41/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    public float speed, jumpForce;
+    public KeyCode left, right, jump;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey(left))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else if (Input.GetKey(jump) && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+		
+	}
+}
