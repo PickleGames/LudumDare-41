4c2b3be8b47f331c97e8d3409262ee7857966112
Howarddful123 <Howarddful123@gmail.com>
2018-04-22 20:41:35 -0700

    -Added new wall and platforms sprites. -Updated spinning saw sprites.

--- LudumDare41/Assets/Scripts/TrapScript.cs
+++ LudumDare41/Assets/Scripts/TrapScript.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class TrapScript : MonoBehaviour {
     public float spinSpeed;
+    public GameObject loseScreen;
+    public WallMechanic wall;
     // Update is called once per frame
     void Update () {
         this.transform.Rotate(0,0,spinSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            wall.moveSpeed = 0;
+            Debug.Log("Wall Stop Moving...");
+            other.gameObject.GetComponent<PlayerMovement>().enabled = false;
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            other.gameObject.GetComponentInChildren<Weapon>().enabled = false;
+            other.gameObject.GetComponentInChildren<ArmRotation>().enabled = false;
+            loseScreen.SetActive(true);
+            Debug.Log("Player Dieeedddd");
+
+
+
+        }
     }
 }
